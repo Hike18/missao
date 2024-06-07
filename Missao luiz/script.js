f2606e45c069d851afa449663cccfdafa631fda4
Hike18 <108475564+Hike18@users.noreply.github.com>
2024-06-07 09:35:46 -0300

    Add files via upload

---
+++ Missao luiz/script.js
@@ -0,0 +1,22 @@
+const caixaprincipal = document.querySelector(".caixa-principal");
+const caixaperguntas = document.querySelector(".caixa-perguntas");
+const caixaalternativas = document.querySelector(".caixa-alternativas");
+const caixaresultado = document.querySelector(".caixa-resultado");
+const textoresultado = document.querySelector(".caixa-resultado");
+
+const perguntas = [
+    {
+        enunciado: "Pergunta 1",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+    {
+        enunciado: "Pergunta 2",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    }
+];
